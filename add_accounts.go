a4587ae7771b082cd235ed9658869def9b4ac3d9
cvirtucio <cvirtucio@mitre.org>
2017-11-10 19:33:55 -0500

    Added add_accounts file.

---
+++ add_accounts.go
@@ -0,0 +1,9 @@
+package caasd_portal_automation_go;
+
+import "database/sql"
+import "github.com/go-sql-driver/mysql"
+import "os/exec"
+
+func main () {
+	// db, err := sql.Open("mysql", "root:1")	
+}
